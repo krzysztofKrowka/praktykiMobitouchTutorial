0c78e845add26819fc6f150621c8b229a4cb03b7
krzysiek.krowka <krzysztof.krowka@onet.eu>
2023-03-07 08:54:20 +0100

    styles: assign code to correct modules with comments

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -5,26 +5,31 @@
         public static Random random = new Random();
         static void Main(string[] args)
         {
-            /*     string Name = "Bob";
-                 Name= Name+ " Bond";
-                 int count = 3;
-                 double temperature = 34.4;
-                 Console.WriteLine($"Hello, {Name}! You  have {count} in your inbox. The temperature is {temperature} celsius.\n");
-                 Console.WriteLine(@"View english output:
-       c:\Exercise\ACME\data.txt
-         ");
-                 Console.WriteLine(@"Посмотреть русский вывод
-       c:\Exercise\ACME\data.txt
-         ");*/
+              //Second module
+              /*string Name = "Bob";
+                Name= Name+ " Bond";
+                int count = 3;
+                double temperature = 34.4;
+                Console.WriteLine($"Hello, {Name}! You  have {count} in your inbox. The temperature is {temperature} celsius.\n");
+                Console.WriteLine(@"View english output:
+               c:\Exercise\ACME\data.txt
+                 ");
+                         Console.WriteLine(@"Посмотреть русский вывод
+               c:\Exercise\ACME\data.txt
+                 ");*/
 
-         /*int fahrenheit = random.Next(1, 73);
-            Console.WriteLine(Math.Floor(((float)fahrenheit - 32) / 9 * 5 * 10) / 10);
-            int firstValue = random.Next(1, 100);
-            int secondValue = random.Next(1, 100);
-            int largerValue = Math.Max(firstValue, secondValue);
-            Console.WriteLine($"First: {firstValue} Second: {secondValue} Larger:{largerValue}");*/
 
-            /*    int roll1=random.Next(1, 7);
+              //Fourth module
+              /*int fahrenheit = random.Next(1, 73);
+                Console.WriteLine(Math.Floor(((float)fahrenheit - 32) / 9 * 5 * 10) / 10);
+                int firstValue = random.Next(1, 100);
+                int secondValue = random.Next(1, 100);
+                int largerValue = Math.Max(firstValue, secondValue);
+                Console.WriteLine($"First: {firstValue} Second: {secondValue} Larger:{largerValue}");*/
+
+
+              //Fifth module
+              /*int roll1=random.Next(1, 7);
                 int roll2=random.Next(1, 7);
                 int roll3=random.Next(1, 7);
                 int score = roll1+roll2+roll3; 
@@ -50,6 +55,8 @@
                 else
                     Console.WriteLine("You win a kitten!");*/
 
+
+              //Sixth module
               /*int daysUntilExpiration = random.Next(12);
                 int discountPercentage = 0;
             
@@ -70,12 +77,19 @@
                 if (daysUntilExpiration > 10)
                     Console.WriteLine();*/
 
+
+              //Seventh module
               /*string[] array = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
                 foreach(string item in array)
                 {
                     if(item.StartsWith("B"))
                         Console.WriteLine(item);
                 }*/
+
+             
+
+
+
         }
     }
 }
